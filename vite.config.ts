9fcfe7c0935176086f0d2e081c69e63ab2fec3fe
CursingAphid <milan.albertz@gmail.com>
2025-10-04 18:14:04 +0200

    sg

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteImagemin from 'vite-plugin-imagemin'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,14 @@ export default defineConfig({
       mozjpeg: { quality: 72, progressive: true },
       svgo: { plugins: [{ name: 'removeViewBox', active: false }] },
       webp: { quality: 70 }
+    }),
+    ViteImageOptimizer({
+      includePublic: true,
+      jpg: { quality: 72, progressive: true },
+      jpeg: { quality: 72, progressive: true },
+      png: { quality: 70 },
+      webp: { quality: 70 },
+      avif: { quality: 50 }
     })
   ]
 })
